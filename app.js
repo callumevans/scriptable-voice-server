1cbb266d4d7d1654574fa398f666612128545898
Callum Evans <callumevans@outlook.com>
2016-09-12 21:52:37 +0100

    Cleanup

--- app.js
+++ app.js
@@ -5,27 +5,6 @@ let logger = require("morgan");
 
 let app = express();
 
-// Error logger
-app.use(logger("dev"));
-
-if (app.get("env") === "development") {
-    app.use(function (error, request, response, next) {
-        response.status(error.status || 500);
-        response.render("error", {
-            message: error.message,
-            error: error
-        });
-    });
-}
-
-app.use((error, request, response, next) => {
-    response.status(error.status || 500);
-    response.render("error", {
-        message: error.message,
-        error: { }
-    });
-});
-
 // 404 Handler
 app.use((request, response, next) => {
     let error = new Error("Not Found");
@@ -33,5 +12,26 @@ app.use((request, response, next) => {
 
     next(error);
 });
+
+// Error logger
+app.use(logger("dev"));
+
+if (app.get("env") === "development") {
+    app.use((error, request, response) => {
+        response.status(error.status || 500);
+        response.render("error", {
+            message: error.message,
+            error: error
+        });
+    });
+} else {
+    app.use((error, request, response) => {
+        response.status(error.status || 500);
+        response.render("error", {
+            message: error.message,
+            error: null
+        });
+    });
+}
 
 module.exports = app;
