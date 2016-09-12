892372a84dff5fc54871c99380d4429845bc3ada
Callum Evans <callumevans@outlook.com>
2016-09-12 04:28:53 +0100

    Error handling

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
+"use strict";
+
 let express = require("express");
+let logger = require("morgan");
 
 let app = express();
+
+// Error logger
+app.use(logger("dev"));
+
+if (app.get("env") === "development") {
+    app.use(function (error, request, response, next) {
+        response.status(error.status || 500);
+        response.render("error", {
+            message: error.message,
+            error: error
+        });
+    });
+}
+
+app.use((error, request, response, next) => {
+    response.status(error.status || 500);
+    response.render("error", {
+        message: error.message,
+        error: { }
+    });
+});
+
+// 404 Handler
+app.use((request, response, next) => {
+    let error = new Error("Not Found");
+    error.status = 404;
+
+    next(error);
+});
 
 module.exports = app;
