e3c1590af041e4f03a3e9645cb2cfb360c79bd58
Callum Evans <callumevans@outlook.com>
2016-09-13 01:15:35 +0100

    Event listeners

--- bin/www.js
+++ bin/www.js
@@ -1,11 +1,24 @@
 "use strict";
 
+// Modules
+
 let app = require("../app");
 let http = require("http");
+
+// Set port
 
 let port = process.env.PORT || "3000";
 app.set("port", port);
 
-let server = http.createServer(app);
+// Create server
 
+let server = http.createServer(app);
 server.listen(port);
+
+// Event listeners
+
+server.on("listening", onListening);
+
+function onListening() {
+    console.log(`Listening on port ${port}`);
+}
